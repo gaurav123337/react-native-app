f703835c436adb0c343c231601aaf4c9e4d1398f
Gaurav <gaurav.123337@gmail.com>
2022-01-17 15:49:27 +0530

    Authentication code added

---
+++ track-server/src/index.js
@@ -0,0 +1,30 @@
+require('./models/Users');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+const PORT = '3000';
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://root:root@cluster0.qoljq.mongodb.net/tracker-server?retryWrites=true&w=majority';
+mongoose.connect(mongoUri);
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongoose');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('ErrorConnected to mongoose', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(PORT, () => {
+  console.log('Server running' + PORT);
+})
