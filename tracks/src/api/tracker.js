12de6eb4353d5a4f24da61b9a3948d5dfcec68c5
Gaurav <gaurav.123337@gmail.com>
2022-02-01 18:01:10 +0530

    track app

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const instance = axios.create({
-  baseURL: 'http://acfd-2409-4043-2313-e0f4-1913-fd42-3596-b57.ngrok.io',
+  baseURL: 'https://mobile-track-server.herokuapp.com/',
 });
 
 instance.interceptors.request.use(
